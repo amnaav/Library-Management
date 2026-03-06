8824d4c4db6f3603dc09ededad552e686d36af4d
amnaav <amnanu589@gmail.com>
2026-03-06 12:10:40 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import Home from "./Components/Home";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Stat from "./Components/Stat";
-import Feature from "./Components/Feature";
-import Cta from "./Components/Cta";
-import Book from "./Components/Book";
-import Category from "./Components/Category";
+import StudentLogin from "./Components/StudentLogin";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/feature" element={<Feature/>}/>
-        <Route path="/book" element={<Book/>}/>
-        <Route path="/stat" element={<Stat/>}/>
-        <Route path="/category" element={<Category/>}/>
+        <Route path="/studentlogin" element={<StudentLogin/>}/>
       </Routes>
-    <Cta/>
     </BrowserRouter>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,47 +1,128 @@
-import { Link } from 'react-router-dom'
-import '../Styles/Home.css'
-import Book from './Book'
-import Feature from './Feature'
-import Stat from './Stat'
-import Category from './Category'
+import { Link } from "react-router-dom";
+import "../Styles/Home.css";
+
 
 const Home = () => {
   return (
     <div>
       <nav className="navbar">
-        <div className="logo">
-          📚Library LMS
-        </div>
-        <ul className="nav-links">
-          <li><Link to={'/'}>Home</Link></li>
-          <li><Link to={'/feature'}>Features</Link></li>
-          <li><Link to={'/book'}>Books</Link></li>
-          <li>Admin Login</li>
-          <li>Student Login</li>
-        </ul>
-        <button className="register-btn">
-          Register
-        </button>
-      </nav>
-    
-      <div>
-        <section className='hero'>
-          <div className='hero-content'>
-            <h1>Digital Library Platform</h1>
-            <p>Discover thousands of books, manage borrowing, and experience a smart digital library.</p>
-            <div className='hero-buttons'>
-              <button className='browse-btn'>Browse books</button>
-              <button className='student-btn'>Student Login</button>
-            </div>
-          </div>
-        </section>
-      </div>
-      <Stat/>
-      <Feature/>
-      <Category/>
-      <Book/>
-    </div>
-  )
-}
+        <div className="logo">📚Library LMS</div>
 
-export default Home
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#feature">Features</a></li>
+          <li><a href="#books">Books</a></li>
+          <li><Link to="/adminlogin">Admin Login</Link></li>
+          <li><Link to="/studentlogin">Student Login</Link></li>
+        </ul>
+
+        <button className="register-btn">Register</button>
+      </nav>
+
+      <section className="hero" id="home">
+        <div className="hero-content">
+          <h1>Digital Library Platform</h1>
+
+          <p>
+            Discover thousands of books, manage borrowing, and experience a
+            smart digital library.
+          </p>
+
+          <div className="hero-buttons">
+            <button className="browse-btn">Browse books</button>
+            <button className="student-btn">Student Login</button>
+          </div>
+        </div>
+      </section>
+      <section className='stats'id ="stats">
+        <div className="stat">
+          📚
+          <h2>5000+</h2>
+          <p>Books Available</p>
+        </div>
+
+        <div className="stat">
+          👩‍🎓
+          <h2>1200+</h2>
+          <p>Students</p>
+        </div>
+
+        <div className="stat">
+          👨‍💻
+          <h2>350+</h2>
+          <p>Daily Readers</p>
+        </div>
+
+        <div className="stat">
+          📘
+          <h2>50+</h2>
+          <p>Categories</p>
+        </div>
+      </section>
+      <section className='features' id ="feature">
+        <h2>Library Features</h2>
+          <div className='feature-grid'>
+            <div className='feature-card'>
+              📚
+              <h3>Smart Book Search</h3>
+              <p>Find books by title,author, and category.</p>
+            </div>
+            <div className='feature-card'>
+              🔄
+              <h3>Easy Issue & Return</h3>
+              <p>Issue and return books digitally.</p>
+            </div>
+            <div className='feature-card'>
+              📊
+              <h3>Admin Dashboard</h3>
+              <p>Monitor library activities & reports.</p>
+            </div>
+            <div className="feature-card">
+            💰
+            <h3>Fine Management</h3>
+            <p>Automated overdue fine tracking.</p>
+          </div>
+        </div>
+      </section>
+      <section className='categories'id ="categories">
+        <h2>Book Categories</h2>
+        <div className='category-grid'>
+          <div className='cat blue'>Programming</div>
+          <div className='cat green'>science</div>
+          <div className='cat red'>Technology</div>
+          <div className='cat yellow'>History</div>
+          <div className='cat purple'>Education</div>
+          <div className='cat orange'>Mathematics</div>
+        </div>
+      </section>
+      <section className='books'id ="books">
+        <h2>Popular Books</h2>
+        <div className='book-grid'>
+          <div className="book-card">
+            <img src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png"/>
+            <h4>React Development</h4>
+          </div>
+          <div className="book-card">
+            <img src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png"/>
+            <h4>Data Structures</h4>
+          </div>
+          <div className="book-card">
+            <img src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png"/>
+            <h4>Machine Learning</h4>
+          </div>
+          <div className="book-card">
+            <img src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png"/>
+            <h4>Artificial Intelligence</h4>
+          </div>
+        </div>
+      </section>
+      <section className='cta'>
+        <h2>Start Your Learning Journey Today</h2>
+        <p>Join our digital library and explore unlimited books</p>
+        <button className='cta-btn'>Register Now</button>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
